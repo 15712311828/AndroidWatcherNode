c822d3cf20ff963dabc1679af9d40297afa3733e
haoyan.zhang <haoyan.zhang@qunar.com>
2018-04-13 14:01:51 +0800

    12

--- public/js/menu.js
+++ public/js/menu.js
@@ -45,7 +45,60 @@ $(function(){
         });
     });
 
+    $('#btn_change_password').click(function(){
+        var password=$('input[name=new-password]').val();
+        var comfirm_password=$('input[name=comfirm-new-password]').val();
+        if(password!=comfirm_password){
+            alert("两次密码不一致");
+            return;
+        }
+        param={};
+        param.password=password;
+        var json=JSON.stringify(param);
+        $.ajax({
+            type: "POST",
+            url: "/api/user/changePassword",
+            dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            data: json,
+        　　success: function(result){
+                if(result.status==0){
+                    alert("成功");
+                    $('input[name=new-password]').val("");
+                    $('input[name=comfirm-new-password]').val("");
+                }
+                else{
+                    alert(result.message);
+                }
+            }
+        });
+    });
+
     loadUserList(1,10);
+
+    $("#next-page").click(function(){
+        UserListPageNum++;
+        loadUserList(UserListPageNum,10);
+    });
+
+    $("#last-page").click(function(){
+        UserListPageNum--;
+        loadUserList(UserListPageNum,10);
+    });
+
+    $("#device-comfirm").click(function(){
+        var device=$('input[name=device-name]').val();
+        var srclink="rtmp://118.89.229.227/live/"+device;
+        var options = {
+            sources: [{
+                src: srclink,
+            }]
+        };
+        var player = videojs('my-player', options, function onPlayerReady() {
+          videojs.log('Your player is ready!');
+          this.play();
+        });
+    });
 });
 function loadUserList(pageNum,pageSize){
     param={};
@@ -65,12 +118,45 @@ function loadUserList(pageNum,pageSize){
                 $('#user-list-table tr[name!=head]').remove();
                 for(var i=0;i<userList.length;i++){
                     var tr=$("<tr/>");
-                    tr.append('<td>'+(UserListPageNum-1)*10+i+'</td>');
+                    tr.append('<td>'+((UserListPageNum-1)*10+i)+'</td>');
                     tr.append('<td>'+userList[i].name+'</td>');
-                    tr.append('<td>'+"删除"+'</td>');
+                    tr.append('<td>'+"<a onclick='deleteUser("+userList[i].id+")'>删除</a>"+'</td>');
                     $('#user-list-table').append(tr);
+                }
+                if(total<=UserListPageNum*10){
+                    $("#next-page").hide();
+                }
+                else{
+                    $("#next-page").show();
+                }
+                if(UserListPageNum==1){
+                    $("#last-page").hide();
+                }
+                else{
+                    $("#last-page").show();
                 }
             }
         }
     });
-}
+};
+function deleteUser(id){
+    param={};
+    param.id=id;
+    var json=JSON.stringify(param);
+    $.ajax({
+        type: "POST",
+        url: "/api/user/delete",
+        dataType: "json",
+        contentType: "application/json;charset=utf-8",
+        data: json,
+    　　success: function(result){
+            if(result.status==0){
+                alert("成功");
+                loadUserList(UserListPageNum,10);
+            }
+            else{
+                alert(result.message);
+            }
+        }
+    });
+};
